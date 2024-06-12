fd4d182473b668f3820a43506342b7860e8b1d7f
Tseretel1 <giorgitsereteli541@gmail.com>
2024-06-12 13:19:08 +0400

    Add project files.

---
+++ Tickets_selling-App/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tickets_selling_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tickets_selling-App/DbContext/Tkt_Dbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tickets_selling_App.Models;
+
+namespace Tickets_selling_App
+{
+    public class Tkt_Dbcontext :DbContext
+    {
+        public Tkt_Dbcontext(DbContextOptions<Tkt_Dbcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
